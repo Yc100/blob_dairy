060753b34e3a23a76881a0466e07ffa1fe8a8bbb
yc100 <1301760713@qq.com>
2023-05-23 17:48:42 +0800

    Site updated: 2023-05-23 17:48:41

---
+++ config/js/weather.js
@@ -0,0 +1,10 @@
+WIDGET = {
+    "CONFIG": {
+      "layout": "1",
+      "width": "450",
+      "height": "150",
+      "background": "1",
+      "dataColor": "FFFFFF",
+      "key": "81b8be975bf346619260d3e2d28772fc"
+    }
+  }
