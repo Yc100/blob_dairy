839eef0f3dbed0aa7f28bce7dba78ad33f522f7e
yc100 <1301760713@qq.com>
2023-07-27 11:06:37 +0800

    Site updated: 2023-07-27 11:06:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/31/20230531-1/","2023/05/26/20230526-1/","2023/06/05/20230605-1/","2023/05/27/20230601-1/","2023/06/08/20230608-1/","2021/06/03/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/26/20230526-1/","2023/05/31/20230531-1/","2023/06/05/20230605-1/","2023/06/08/20230608-1/","2023/05/27/20230601-1/","2021/06/03/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
